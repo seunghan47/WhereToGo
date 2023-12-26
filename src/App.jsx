67eb1980ff0051bb6af8079fbc19bd7c10b2fec9
Paul Lim <paulslim47@gmail.com>
2023-12-26 12:56:18 -0500

    initializaing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useRef, useState } from 'react';
-
 import Places from './components/Places.jsx';
 import { AVAILABLE_PLACES } from './data.js';
 import Modal from './components/Modal.jsx';
